e007b45068dec3d9ff61ef93f9355b0e2f3ae85c
Grant Murphy <grantsmurphy@gmail.com>
2018-12-27 21:22:03 -0500

    first commit

---
+++ src/main/java/com/example/thymeleaflogin/thymeleaftlogin/ThymeleaftloginApplication.java
@@ -0,0 +1,14 @@
+package com.example.thymeleaflogin.thymeleaftlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleaftloginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleaftloginApplication.class, args);
+    }
+
+}
+
