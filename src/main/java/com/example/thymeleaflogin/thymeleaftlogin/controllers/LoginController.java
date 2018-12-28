352a9d07ed118de3506439773d73dcf93a3ab86f
Grant Murphy <grantsmurphy@gmail.com>
2018-12-27 21:57:02 -0500

    wire up the login controller

--- src/main/java/com/example/thymeleaflogin/thymeleaftlogin/controllers/LoginController.java
+++ src/main/java/com/example/thymeleaflogin/thymeleaftlogin/controllers/LoginController.java
@@ -1,10 +1,36 @@
 package com.example.thymeleaflogin.thymeleaftlogin.controllers;
 
+import com.example.thymeleaflogin.thymeleaftlogin.model.LoginForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
 
+        //Get login form page
+        @RequestMapping(value ="/login", method = RequestMethod.GET)
+        public String getLoginForm(){
+            //return html page name
+            return "login";
+        }
+
+        //checking login creds
+        @RequestMapping(value="/login", method =RequestMethod.POST)
+        public String login(@ModelAttribute(name = "loginForm")LoginForm loginForm, Model model) {
+            String userName = loginForm.getUserName();
+            String password = loginForm.getPassword();
+            if("admin".equals(userName) && "admin".equals(password)) {
+                //if userName and pass are correct we return to home page
+                return "home";
+            }
+            //If username or pass are wrong
+            model.addAttribute("invalidCredentials", true);
+            //return again login page
+            return "login";
+        }
 
 
 
---
+++ src/main/java/com/example/thymeleaflogin/thymeleaftlogin/model/LoginForm.java
@@ -0,0 +1,27 @@
+package com.example.thymeleaflogin.thymeleaftlogin.model;
+
+public class LoginForm {
+
+    private String userName;
+    private String password;
+
+    public LoginForm() {
+        super();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
